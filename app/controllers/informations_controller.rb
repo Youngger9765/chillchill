7c9b439c56ef6ee630b48c366887f2cac5683940
purpleice9765 <purpleice9765@msn.com>
2015-11-20 19:28:55 +0800

    partner's information create

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -1,7 +1,31 @@
 class InformationsController < ApplicationController
 
   def index
-    
+    @partner = Partner.find(params[:partner_id])
+    @information = Information.new
+    @informations = @partner.informations
   end
+
+  def create
+    @information = Information.create(information_params)
+    PartnerInformationShip.create(:partner_id => params[:partner_id], :information_id => @information.id)
+    
+    if @information.save
+      flash[:notice] = "Create Success!"
+      redirect_to informations_path(:partner_id => params[:partner_id])
+    else
+      flash[:alert] = "Create fail!"
+      render "index"
+    end
+  end
+
+  private
+
+  def information_params
+    params.require(:information).permit(:address, :en_address, :company, :en_company,
+                                    :open_day, :en_open_day, :open_time, :close_time
+                                    )
+  end
+
 
 end
