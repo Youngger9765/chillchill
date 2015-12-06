266931167d54bb2b27725d55a86de47836eb857f
purpleice9765 <purpleice9765@msn.com>
2015-12-06 15:52:28 +0800

    registered form basic function

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
 
-  before_action :find_event, :only => [:show, :update, :destroy]
+  before_action :find_event, :only => [:show, :update, :destroy, :registration_form]
 
   def index
     @events = Event.all
@@ -14,14 +14,33 @@ class EventsController < ApplicationController
   end
 
   def show
-    
+    @registration_form = RegistrationForm.new
   end
 
+  def registration_form
+    @registration_form = RegistrationForm.new(registration_form_params)
+    @registration_form.event_id = @event.id
+
+    if @registration_form.save
+      flash[:notice] = "Create Success!"
+      redirect_to project_event_path(:project_id => params[:project_id],:id => @event.id)
+    else
+      flash[:alert] = "Create fail!"
+      render "show"
+    end
+  end
 
   private
 
   def find_event
     @event = Event.find(params[:id])
+  end
+
+  def registration_form_params
+    params.require(:registration_form).permit( :event_id, :first_name,:last_name,
+                                               :phone_number, :email, :people, :kids,
+                                               :registed_day, :payment, :notice, :opinion
+                                              )
   end
 
 
--- app/models/registration_form.rb
+++ app/models/registration_form.rb
@@ -1,5 +1,27 @@
 class RegistrationForm < ActiveRecord::Base
 
+  validates_presence_of :first_name, :phone_number, :email, :people, :payment ,:message => "請確認 * 必填"
+  validates_length_of :first_name, :last_name, :minimum => 2
+  validates_numericality_of :phone_number,:people, :only_integer => true
+  validates_format_of :email, :with => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  
   belongs_to :event
+
+  def day_list(event)
+    event.reservation_days.map{ |d| d.day_info }
+  end
+
+  def day_list=(str)
+    arr = str.split(",")
+
+    self.reservation_days = arr.map do |d|
+      reservation_day = ReservationRay.find_by_day_info(d)
+      unless reservation_day
+        reservation_day = ReservationDay.create!( :day_info => d )
+      end
+      reservation_day
+    end
+  end
+
 
 end
